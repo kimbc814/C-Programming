b03d7565d6250ea8a03c79626846a42b61ad1f35
bc_01 <46893535+kimbc814@users.noreply.github.com>
2023-07-21 21:12:06 +0900

    Update TF_Rotation.c

--- TF-Rotation/TF_Rotation.c
+++ TF-Rotation/TF_Rotation.c
@@ -29,41 +29,7 @@ double g_Rotation_matrix[2][2];
 double g_Rotation_matrix_inverse[2][2];
 
 ///////////////////////////////////base -> map///////////////////////////////////////
-//각 계산
-void Set_rotation_matrix_inverse(double m_angle_degree){
 
-	g_angle_radian = DEG2RAD(m_angle_degree);
-	g_Rotation_matrix_inverse[0][0] = cos(g_angle_radian);   g_Rotation_matrix_inverse[0][1] = sin(g_angle_radian);
-	g_Rotation_matrix_inverse[1][0] = -sin(g_angle_radian);  g_Rotation_matrix_inverse[1][1] = cos(g_angle_radian);
-
-	printf("Set Rotation_Inverse : ");
-	printf("\n");
-	printf("%6.3lf  %6.3lf\n", g_Rotation_matrix_inverse[0][0], g_Rotation_matrix_inverse[0][1]);
-	printf("%6.3lf  %6.3lf\n", g_Rotation_matrix_inverse[1][0], g_Rotation_matrix_inverse[1][1]);
-}
-//회전
-void TF_base_link_base_link_map(const Point2D* kBase_link_Point2d, Point2D* p_base_link_map_point2d, const Pose2D* kBase_link_origin) {
-
-	Set_rotation_matrix_inverse(kBase_link_origin->theta);
-	p_base_link_map_point2d->x = (g_Rotation_matrix_inverse[0][0] * kBase_link_Point2d->x) + (g_Rotation_matrix_inverse[0][1] * kBase_link_Point2d->y);
-	p_base_link_map_point2d->y = (g_Rotation_matrix_inverse[1][0] * kBase_link_Point2d->x) + (g_Rotation_matrix_inverse[1][1] * kBase_link_Point2d->y);
-
-
-}
-//병진
-void TF_base_link_map_map(Point2D* base_link_map_2d, const Pose2D* base_link_origin) {
-
-	base_link_map_2d->x += (base_link_origin->x);
-	base_link_map_2d->y += (base_link_origin->y);
-}
-//통합
-void TF_base_link_map(const Point2D* kBase_link_Point2d, Point2D* p_base_link_map_point2d, const Pose2D* kBase_link_origin) {
-	TF_base_link_base_link_map(kBase_link_Point2d, p_base_link_map_point2d, kBase_link_origin);
-	TF_base_link_map_map(p_base_link_map_point2d, kBase_link_origin);
-	printf("base_link_map_Point2D.x : %6.3lf\n", base_link_map_Point2D.x);
-	printf("base_link_map_Point2D.y : %6.3lf\n\n", base_link_map_Point2D.y);
-}
-///////////////////////////////////map -> base///////////////////////////////////////
 //각 계산
 void Set_rotation_matrix(double m_angle_degree) {
 
@@ -77,28 +43,64 @@ void Set_rotation_matrix(double m_angle_degree) {
 	printf("%6.3lf  %6.3lf\n", g_Rotation_matrix[1][0], g_Rotation_matrix[1][1]);
 }
 //회전
-void TF_base_link_map_base_link(Point2D* p_base_link_Point2d, const Point2D* kBase_link_map_point2d, const Pose2D* kBase_link_origin) {
+void TF_base_link_base_link_map(const Point2D* kBase_link_Point2d, Point2D* p_Base_link_map_point2d, const Pose2D* kBase_link_origin) {
 
 	Set_rotation_matrix(kBase_link_origin->theta);
-	p_base_link_Point2d->x = (g_Rotation_matrix[0][0] * kBase_link_map_point2d->x) + (g_Rotation_matrix[0][1] * kBase_link_map_point2d->y);
-	p_base_link_Point2d->y = (g_Rotation_matrix[1][0] * kBase_link_map_point2d->x) + (g_Rotation_matrix[1][1] * kBase_link_map_point2d->y);
-
-
+	p_Base_link_map_point2d->x = (g_Rotation_matrix[0][0] * kBase_link_Point2d->x) + (g_Rotation_matrix[0][1] * kBase_link_Point2d->y);
+	p_Base_link_map_point2d->y = (g_Rotation_matrix[1][0] * kBase_link_Point2d->x) + (g_Rotation_matrix[1][1] * kBase_link_Point2d->y);
 }
 //병진
-void TF_map_base_link_map(Point2D* base_link_map_2d, const Pose2D* base_link_origin) {
+void TF_base_link_map_map(Point2D* p_base_link_map_2d, const Pose2D* base_link_origin) {
 
-	base_link_map_2d->x -= (base_link_origin->x);
-	base_link_map_2d->y -= (base_link_origin->y);
+	p_base_link_map_2d->x += (base_link_origin->x);
+	p_base_link_map_2d->y += (base_link_origin->y);
 }
 
 //통합
-void TF_map_base_link(Point2D* p_base_link_Point2d, Point2D* p_Base_link_map_point2d, const Pose2D* kBase_link_origin) {
-	TF_map_base_link_map(p_Base_link_map_point2d, kBase_link_origin);
-	TF_base_link_map_base_link(p_base_link_Point2d, p_Base_link_map_point2d, kBase_link_origin);
+void TF_map_base_link( const Point2D* kbase_link_Point2d, Point2D* p_Base_link_map_point2d, const Pose2D* kBase_link_origin) {
+	TF_base_link_base_link_map(kbase_link_Point2d, p_Base_link_map_point2d, kBase_link_origin);
+    TF_base_link_map_map(p_Base_link_map_point2d, kBase_link_origin);
+	printf("base_link_map_Point2D.x : %6.3lf\n", base_link_map_Point2D.x);
+	printf("base_link_map_Point2D.y : %6.3lf\n\n", base_link_map_Point2D.y);
+}
+/////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////map -> base///////////////////////////////////////
+
+//각 계산
+void Set_rotation_matrix_inverse(double m_angle_degree){
+
+	g_angle_radian = DEG2RAD(m_angle_degree);
+	g_Rotation_matrix_inverse[0][0] = cos(g_angle_radian);   g_Rotation_matrix_inverse[0][1] = sin(g_angle_radian);
+	g_Rotation_matrix_inverse[1][0] = -sin(g_angle_radian);  g_Rotation_matrix_inverse[1][1] = cos(g_angle_radian);
+
+	printf("Set Rotation_Inverse : ");
+	printf("\n");
+	printf("%6.3lf  %6.3lf\n", g_Rotation_matrix_inverse[0][0], g_Rotation_matrix_inverse[0][1]);
+	printf("%6.3lf  %6.3lf\n", g_Rotation_matrix_inverse[1][0], g_Rotation_matrix_inverse[1][1]);
+}
+//병진
+void TF_map_base_link_map(Point2D* p_base_link_map_2d, const Pose2D* base_link_origin) {
+
+	p_base_link_map_2d->x -= (base_link_origin->x);
+	p_base_link_map_2d->y -= (base_link_origin->y);
+}
+//회전
+void TF_base_link_map_base_link( Point2D* p_Base_link_Point2d,const Point2D* kbase_link_map_point2d, const Pose2D* kBase_link_origin) {
+
+	Set_rotation_matrix_inverse(kBase_link_origin->theta);
+	p_Base_link_Point2d->x = (g_Rotation_matrix_inverse[0][0] * kbase_link_map_point2d->x) + (g_Rotation_matrix_inverse[0][1] * kbase_link_map_point2d->y);
+	p_Base_link_Point2d->y = (g_Rotation_matrix_inverse[1][0] * kbase_link_map_point2d->x) + (g_Rotation_matrix_inverse[1][1] * kbase_link_map_point2d->y);
+
+
+}
+//통합
+void TF_base_link_map(Point2D* p_Base_link_Point2d, Point2D* p_base_link_map_point2d, const Pose2D* kBase_link_origin) {
+	TF_map_base_link_map(p_base_link_map_point2d, kBase_link_origin);
+    TF_base_link_map_base_link(p_Base_link_Point2d, p_base_link_map_point2d, kBase_link_origin);
 	printf("base_link_Point2D.x : %6.3lf\n", base_link_Point2D.x);
 	printf("base_link_Point2D.y : %6.3lf\n\n", base_link_Point2D.y);
 }
+/////////////////////////////////////////////////////////////////////////////////////
 
 int _tmain(int argc, _TCHAR* argv[]){
 
@@ -128,10 +130,10 @@ int _tmain(int argc, _TCHAR* argv[]){
 
 	//TF_BaseLinkMap2BaseLink(&base_link_Point2D, &base_link_map_Point2D, &base_link_origin);
 	puts("=======================Rotation==========================\n");
-	TF_base_link_map(&base_link_Point2D, &base_link_map_Point2D, &base_link_origin);
+	TF_map_base_link(&base_link_Point2D, &base_link_map_Point2D, &base_link_origin);
 	//base_link_map_Point2D.x = 6.000;
 	//base_link_map_Point2D.y = 5.000;
-	TF_map_base_link(&base_link_Point2D, &base_link_map_Point2D, &base_link_origin);
+	TF_base_link_map(&base_link_Point2D, &base_link_map_Point2D, &base_link_origin);
 	puts("=========================================================");
 	return 0;
 }
