9a65a5c7c5d4f0ac92eff0cdaf86f7545248fb55
bc_01 <46893535+kimbc814@users.noreply.github.com>
2024-01-10 20:26:12 +0900

    Update RLC_AC_circuit.cpp

--- 2024/practice_problem/RLC_AC_circuit.cpp
+++ 2024/practice_problem/RLC_AC_circuit.cpp
@@ -1,58 +1,85 @@
 #define _USE_MATH_DEFINES
-
 #include<iostream>
-#include <math.h>
+#include<cmath>
 
-using namespace std;
+class RLC_Circuit {
+private:
+    double R;
+    double L;
+    double C;
+    double frequency;
+    double V_p;
 
-void main() {
-	double R = 0.0;
-	double L = 0.0;
-	double C = 0.0;
-	double frequency = 0.0;
-	double V_p = 0.0;
+    double X_L;
+    double X_C;
+    double Z;
 
-	double X_L = 0.0;
-	double X_C = 0.0;
-	double Z = 0.0;
-	
-	double I_p = 0.0;
-	double phase_angle = 0.0;
-	
-	cout << "input R (ohm): ";
-	cin >> R;
+    double I_p;
+    double phase_angle;
 
-	cout << "input L (Henry): ";
-	cin >> L;
+    void calculateParameters() {
+        X_L = 2 * M_PI * frequency * L;
+        X_C = 1.0 / (2 * M_PI * frequency * C);
+        Z = sqrt(pow(R, 2) + pow(X_L - X_C, 2));
+        I_p = V_p / Z;
+        phase_angle = 180.0 / M_PI * atan((X_L - X_C) / R);
+    }
 
-	cout << "input C (micro Farad): ";
-	cin >> C;
-	C = C / pow(10,6);
-	
-	cout << "input frequency : ";
-	cin >> frequency;
+public:
+    RLC_Circuit() {
+        R = 0.0;
+        L = 0.0;
+        C = 0.0;
+        frequency = 0.0;
+        V_p = 0.0;
 
-	cout << "input V_p : ";
-	cin >> V_p;
-	
-	X_L = 2 * M_PI * frequency * L;
-	X_C = 1.0 / (2 * M_PI * frequency * C);
-	Z = sqrt(pow(R, 2) + pow(X_L - X_C, 2));
-	I_p = V_p / Z;
-	phase_angle =180.0/M_PI * atan( (X_L - X_C) / R);
+        X_L = 0.0;
+        X_C = 0.0;
+        Z = 0.0;
 
+        I_p = 0.0;
+        phase_angle = 0.0;
+    }
+    ~RLC_Circuit() {
+    }
+    void getInput() {
+        std::cout << "input R (ohm): ";
+        std::cin >> R;
 
-	printf("X_L = 2 * PI * frequency * L =  %0.3lf(ohm)\n", X_L);
-	printf("X_C =1 / (2 * PI * frequency * C) = %0.3lf(ohm)\n", X_C);
-	printf("Z = (R^2 + (X_L -X_C)^2 )^(1/2) = %0.3lf(ohm)\n", Z);
+        std::cout << "input L (Henry): ";
+        std::cin >> L;
 
-	printf("I_p = V_p / Z = %0.3lf (A)\n", I_p);
-	printf("phase angle = atan((X_L-X_C) / R) = %0.1lf(deg)\n", phase_angle);
+        std::cout << "input C (micro Farad): ";
+        std::cin >> C;
+        C = C / pow(10, 6);
 
+        std::cout << "input frequency : ";
+        std::cin >> frequency;
 
-	printf("V_R최대 = I_최대 * R = (%0.3lf ohm)(%0.3lf ohm) = %0.2lf V\n", I_p, R, I_p * R);
-	printf("V_L최대 = I_최대 * L = (%0.3lf ohm)(%0.3lf ohm) = %0.2lf V\n", I_p, X_L, I_p * X_L);
-	printf("V_C최대 = I_최대 * C = (%0.3lf ohm)(%0.3lf ohm) = %0.2lf V\n", I_p, X_C, I_p * X_C);
+        std::cout << "input V_p : ";
+        std::cin >> V_p;
+    }
 
+    void displayResults() {
+        circuit.calculateParameters();
+        std::printf("X_L = 2 * PI * frequency * L =  %0.3lf(ohm)\n", X_L);
+        std::printf("X_C =1 / (2 * PI * frequency * C) = %0.3lf(ohm)\n", X_C);
+        std::printf("Z = (R^2 + (X_L -X_C)^2 )^(1/2) = %0.3lf(ohm)\n", Z);
+
+        std::printf("I_p = V_p / Z = %0.3lf (A)\n", I_p);
+        std::printf("phase angle = atan((X_L-X_C) / R) = %0.1lf(deg)\n", phase_angle);
+
+        std::printf("V_R최대 = I_최대 * R = (%0.3lf ohm)(%0.3lf ohm) = %0.2lf V\n", I_p, R, I_p * R);
+        std::printf("V_L최대 = I_최대 * L = (%0.3lf ohm)(%0.3lf ohm) = %0.2lf V\n", I_p, X_L, I_p * X_L);
+        std::printf("V_C최대 = I_최대 * C = (%0.3lf ohm)(%0.3lf ohm) = %0.2lf V\n", I_p, X_C, I_p * X_C);
+    }
+};
+
+int main() {
+    RLCircuit circuit;
+    
+    circuit.getInput();
+    circuit.displayResults();
+
+    return 0;
 }
-
